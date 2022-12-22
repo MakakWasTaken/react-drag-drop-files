99c7b0f5b3b9820a2781716a859b501be43c103a
Markus Moltke <makakwastaken@gmail.com>
2022-12-22 22:31:16 +0100

    Yet another attempt at making package work

---
+++ rollup.config.js
@@ -0,0 +1,35 @@
+const dts = require('rollup-plugin-dts');
+const esbuild = require('rollup-plugin-esbuild');
+
+const name = require('./package.json').main.replace(/\.js$/, '');
+
+const bundle = (config) => ({
+  ...config,
+  input: './src/index.ts',
+  external: (id) => !/^[./]/.test(id)
+});
+
+module.exports = [
+  bundle({
+    plugins: [esbuild.default()],
+    output: [
+      {
+        file: `${name}.js`,
+        format: 'cjs',
+        sourcemap: true
+      },
+      {
+        file: `${name}.mjs`,
+        format: 'es',
+        sourcemap: true
+      }
+    ]
+  }),
+  bundle({
+    plugins: [dts.default()],
+    output: {
+      file: `${name}.d.ts`,
+      format: 'es'
+    }
+  })
+];
